f47aa8ad41164ffcbd1215220f7f5f09d2292270
Alexander Yemm <alexyemm@gmail.com>
2020-12-28 19:14:17 +0000

    more stuff

--- main.ps1
+++ main.ps1
@@ -3,10 +3,16 @@ $tesseract = "C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 Write-Host $PSScriptRoot
 
-$input = "\input\test.jpg"
-$output = "\output\test"
+$input = "\input\example5.png"
+$output = "\output\example5"
 . $tesseract -l eng $PSScriptRoot$input $PSScriptRoot$output
 
 $text = Get-Content $PSScriptRoot$output".txt"
 
 Write-Host $text
+
+
+$test = $text | Select-String "(?:(?:(DOOR|MEDIPACK|BOX|LOCKER|TERMINAL|ZONE|GENERATOR|AMMOPACK))+)[_ ](\d+)" -CaseSensitive
+
+$test = $text | Select-String "([A-Z_]+)[_ ](\d+)" -CaseSensitive
+ForEach($x in $test.matches){write-host $x.groups[2]}
